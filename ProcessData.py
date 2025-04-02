e6f09fc2b1ef25113ba583b78a905a66032e629e
adamthies17 <adamthies17@gmail.com>
2025-04-02 22:59:46 +0000

    done with lab

--- ProcessData.py
+++ ProcessData.py
@@ -12,12 +12,45 @@ def main():
   outFile = open("StudentList.csv", 'w')
 
   #Process each line of the input file and output to the CSV file
-
-
+  #line = inFile.readline()
+  for line in inFile:
+    data = line.split()
+    student_id = makeID(data[0], data[1], data[3], data[6], data[5])
+    output = data[1] + "," + data[0] + "," + student_id + "\n"
+    outFile.write(output)
+    #print(student_id)
 
   #Close files in the end to save and ensure they are not damaged.
   inFile.close()
   outFile.close()
+
+def makeID(first, last, idNum, major, year):
+  #print(first, last, idNum)
+  idLen = len(idNum)
+
+  while len(last) < 5:
+    last = last + "X"
+
+  major = major.upper()
+
+
+  
+  if year == "Freshman":
+    year = "FR"
+  elif year == "Sophomore":
+    year = "SO"
+  elif year == "Junior":
+    year = "JR"
+  elif year == "Senior":
+    year = "SR"
+
+  id = first[0] + last + idNum[idLen - 3: ] + major[0:3] + "-" + year
+  
+  #print(id)
+  return id
+   
+
+
 
 if __name__ == '__main__':
   main()
